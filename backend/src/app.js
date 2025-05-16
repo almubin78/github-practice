5f97e3adf75d408ca8691c47d64dfac9a4e3abd6
mubin44e <almubin78@gmail.com>
2025-05-16 22:52:18 +0600

    app.js and database connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+
 const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-module.exports = app;
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+console.log(process.env.MONGO_URI);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+// // Start Server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+module.exports = app
